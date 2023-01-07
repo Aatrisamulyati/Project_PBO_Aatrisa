69280464226caba6ee560fd5861bda3672adcfa4
Aatrisamulyati <114573287+Aatrisamulyati@users.noreply.github.com>
2023-01-07 11:03:44 +0700

    Add files via upload

---
+++ Tisa07122022/ExampleException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tisa07122022;
+
+/**
+ *
+ * @author hp
+ */
+public class ExampleException {
+    public static void main( String[] args ){
+        try{
+            System.out.println( args[0] );
+        }catch( ArrayIndexOutOfBoundsException ex ){
+            System.out.println("index ke " +ex.getMessage()+ " Tidak ada");
+        }
+    }    
+}
