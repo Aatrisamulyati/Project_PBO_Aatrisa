c53a14a1056c5ecd2e389337ff5ebbccb0dae22b
Aatrisamulyati <114573287+Aatrisamulyati@users.noreply.github.com>
2023-01-26 02:04:04 +0700

    Add files via upload

---
+++ Teori_PBO_AatrisaMulyati_2101092024/Modul 3/AatrisaMulyati.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tisap23092022;
+
+/**
+ *
+ * @author hp
+ */
+public class AatrisaMulyati {
+     public static void main(String[] args) {
+     System.out.println("Welcome To Java Programming AATRISA MULYATI");
+    }
+}
